3d3308c9e77c6f9abf61d01b9f91b1a9df900c3c
tanvirr007 <43490582+tanvirr007@users.noreply.github.com>
2026-07-18 04:35:35 +0600

    chore: bump version to 3.3
    Bumps the base versionName from 3.2 to 3.3 in build.gradle.kts.
    
    Change-Id: I4b5e4488f225467a9382b223c278297a0c8d104h
    Signed-off-by: tanvirr007 <43490582+tanvirr007@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "3.2"
+        versionName = "3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
