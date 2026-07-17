fa333ad15ce457d12450a905f0a26c72ae819f4b
tanvirr007 <43490582+tanvirr007@users.noreply.github.com>
2026-07-18 04:12:52 +0600

    chore: bump version to 3.0
    Bumps the base versionName from 2.2 to 3.0 in build.gradle.kts ahead of version 3.0 releases.
    
    Change-Id: I3b5e4488f225467a9382b223c278297a0c8d104e
    Signed-off-by: tanvirr007 <43490582+tanvirr007@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "2.2"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
